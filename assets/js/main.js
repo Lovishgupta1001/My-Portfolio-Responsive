7a54266f7123155cf785a9c3c10f1cdb39a8e646
lovishgupta1001@gmail.com <93213511+Lovishgupta1001@users.noreply.github.com>
2022-06-04 23:00:14 +0530

    portfolio

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+// menu show y hidden
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose= document.getElementById('nav-close')
+
+// menu show
+// validate if constant exists
+if(navToggle){
+   navToggle.addEventListener('click', () =>{
+      navMenu.classList.add('show_menu')
+   })
+}
+// menu hidden
+// validate if constant exists
+if(navClose){
+   navClose.addEventListener('click',() => {
+      navMenu.classList.remove('show-menu')
+   })
+}
+
+// remove menu mobile
+
+
+// accordion skills
+
+// qualification tabs
+
+// services modal
