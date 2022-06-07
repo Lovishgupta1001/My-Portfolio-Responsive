15e1d58737958862b0ad2c5f927a5cb28ce06536
lovishgupta1001@gmail.com <93213511+Lovishgupta1001@users.noreply.github.com>
2022-06-07 10:01:58 +0530

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -73,7 +73,7 @@ function carousel() {
   slideIndex++;
   if (slideIndex > x.length) {slideIndex = 1}
   x[slideIndex-1].style.display = "block";
-  setTimeout(carousel, 3000); // Change image every 2 seconds
+  setTimeout(carousel, 2000); // Change image every 2 seconds
 }
 /*==================== ACCORDION SKILLS ====================*/
 
