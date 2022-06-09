06cddb369976e8edda36b7ed701adc230a93d8f4
lovishgupta1001@gmail.com <93213511+Lovishgupta1001@users.noreply.github.com>
2022-06-09 12:54:22 +0530

    commit

--- assets/php/main.php
+++ assets/php/main.php
@@ -4,10 +4,10 @@
 $name = $_POST['name'];
 $email= $_POST['email'];
 $message= $_POST['message'];
-$to = "youremail@mail.com";
-$subject = "Mail From website";
+$to = "lovishgupta1001@mail.com";
+$subject = "Mail From Lovish's Portfolio Website";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: noreply@yoursite.com" . "\r\n" .
+$headers = "From: noreply@lovishgupta1001.github.io/My-Portfolio-Responsive/" . "\r\n" .
 "CC: somebodyelse@example.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
