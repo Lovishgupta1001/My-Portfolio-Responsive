8f5ff65b9caa1a2180e23348b103fb269e27a1bc
lovishgupta1001 <lovishgupta1001@gmail.com>
2023-06-28 00:19:22 +0530

    deployments

--- Server/app.js
+++ Server/app.js
@@ -27,18 +27,14 @@ app.post('/send-email', upload.none(), (request, response) => {
         port: 587,
         secure: false,
         auth: {
-            // user: process.env.EMAIL,
-            // pass: process.env.PASSWORD,
-            user: 'lovishgupta1001@gmail.com',
-            pass: 'ivriamgbuqnncwoe',
+            user: process.env.EMAIL,
+            pass: process.env.PASSWORD,
         },
     });
 
     const mailOptions = {
-        // from: process.env.FROM,
-        // to: process.env.TO,
-        from: 'lovishgupta1001@gmail.com',
-        to: 'lovishgupta1001@gmail.com',
+        from: process.env.FROM,
+        to: process.env.TO,
         subject: 'Enquiry Mail',
         html: `<h2>Dear Lovish</h2><br>
             You have received a new enquiry from:<br>
